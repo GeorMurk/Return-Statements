dd62ac35e6c8126d0e42a54bc0be24bd0b53e34c
George Kiragu <45111839+GeorMurk@users.noreply.github.com>
2019-02-02 01:48:48 +0300

    Add files via upload

---
+++ ReturnStatements.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+#!Giving return statements
+
+def square(number):
+    return number*number
+
+print(square(9))
+print(square(10))
+
+#!Adding a variable
+result = square(45)
+print(result)
+
+#!Go ahead and test it out!!!
